c7f4e2de3c0d7e54d95a0e514bc06d6cb34eccaf
강지원 <athena020219@naver.com>
2024-07-19 12:39:00 +0900

    add .gitignore ...etc
    design: TodoInput add css
    refactor: separate clearInput

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
+    <div class="inputBox shadow">
         <input type="text" v-model="newTodoItem">
-        <button v-on:click="addTodo">add</button>
+        <button v-on:click="addTodo">+</button>
     </div>
 </template>
 
@@ -15,6 +15,9 @@
         methods: {
             addTodo: function() {
                 localStorage.setItem(this.newTodoItem, this.newTodoItem);
+                this.clearInput();
+            },
+            clearInput: function() {
                 this.newTodoItem = '';
             }
         }
@@ -22,5 +25,28 @@
 </script>
 
 <style scoped>
-
+input:focus {
+    outline: none;
+}
+.inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+.inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+}
+.addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+.addBtn {
+    color: white;
+    vertical-align: middle;
+}
 </style>
